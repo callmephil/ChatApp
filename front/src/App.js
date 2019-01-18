a5790e81eabc2144d260c69e4ec51041960e001f
Callmephil <phil19-94@hotmail.fr>
2019-01-18 13:15:31 +0200

    front/App.js: Completed Addition + Message Transmission @Todo Cleanup and styling

--- front/src/App.js
+++ front/src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   state = {
     isConnected:false,
     id:null,
-    peeps: []
+    peeps: [],
+    old_messages: [],
+    answer: "" 
   }
   socket = null
 
@@ -22,7 +24,10 @@ class App extends Component {
     })
 
     /** this will be useful way, way later **/
-    this.socket.on('room', old_messages => console.log(old_messages))
+    this.socket.on('room', (messages) => {
+      console.log(messages)
+      this.setState({old_messages:messages})
+    })
 
     this.socket.on('pong!',(additionalStuff)=>{
       console.log('Phil is testing', additionalStuff)
@@ -39,6 +44,12 @@ class App extends Component {
     this.socket.on('next',(message_from_server) =>
       console.log(message_from_server)
     )
+
+    this.socket.on('addition',(message_from_server) =>
+      console.log(message_from_server)
+    )
+
+    this.socket.emit('addition');
   }
 
   componentWillUnmount(){
@@ -47,6 +58,8 @@ class App extends Component {
   }
 
   render() {
+    const test = {text: "Phil is searching the answer", id: this.state.id, name: "Phil"};
+    // const {date, id, name, text} = this.state.old_messages;
     return (
       <div className="App">
         <div>status: {this.state.isConnected ? 'connected' : 'disconnected'}</div>
@@ -55,9 +68,25 @@ class App extends Component {
         <button onClick={()=>this.socket.emit('give me next')}>Give me next</button>
         <h4> Hello: {this.state.id} </h4>
         <ul> {this.state.peeps ? this.state.peeps.map(x => <li key={x}> {x} </li>) : "null" }</ul>
+        
+        <button onClick={()=>this.socket.emit("message", test)}>Send a message</button>
+        <div>
+          {console.log("Test", this.state.old_message ? this.state.old_messages : "null")}
+          {/* {this.state.old_messages.map(msg => {
+            <div key={msg.id}> {msg.text} </div>
+          })} */}
+        </div>
+
+        <input type="text" name="answer" onChange={e => this.setState({ answer: e.target.value })} />
+        <button onClick={()=>this.socket.emit('answer',this.state.answer)}>submit answer</button>
+
       </div>
     );
   }
 }
+
+/*
+please send the message "answer" with the answer to 90 + 25 + 16. You may send the answer as a string. You may also get a hint.
+*/
 
 export default App;
